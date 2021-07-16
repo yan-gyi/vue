efef0c8101cf4fc399eebb2cdb2b082ed5b566e2
yan-gyi <yanmyoaung.mr@gmail.com>
2021-07-17 03:35:25 +0630

    basic click event

--- js/app.js
+++ js/app.js
@@ -2,23 +2,23 @@ const smallData = {
 	title : "Anatomy",
 	author : "Doctor",
 	age : 50,
+	
 }
 
 //const increaseAge  = function(){alert("hi")};
 //console.log(increaseAge());
+const mtds = {
+		increaseAge(age){this.age++},
+	}
 const data = {
 	//template : " My name is Yan Myo Aung. ",
 	data : function(){ return smallData},
-	methods : {
-		increaseAge(){
-			alert("hi")
-		}
-	},
+	methods : mtds,
 }	
-
 
 const app = Vue.createApp(data);
 
 app.mount("#app");
 
 //console.log(app)
+
